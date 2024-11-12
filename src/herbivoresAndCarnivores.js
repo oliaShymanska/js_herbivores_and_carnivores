8f092e71ebfc2fd344d4f71607645cf42a4eb080
Olia Shymanska <oliaa.shymanska@gmail.com>
2024-11-12 17:07:42 +0200

    change bite method logic and  condition deleting dead animals

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -26,17 +26,15 @@ class Herbivore extends Animal {
 
 class Carnivore extends Animal {
   bite(victim) {
-    if (victim instanceof Carnivore || victim.hidden === true) {
-      return;
+    if (
+      victim instanceof Animal &&
+      victim.hasOwnProperty('hidden') &&
+      !victim.hidden
+    ) {
+      victim.health -= 50;
     }
 
-    victim.health -= 50;
-
-    if (victim.health <= 0) {
-      const indexDeadAnimal = Animal.alive.indexOf(victim);
-
-      Animal.alive.splice(indexDeadAnimal, 1);
-    }
+    Animal.alive = Animal.alive.filter((animal) => animal.health > 0);
   }
 }
 
